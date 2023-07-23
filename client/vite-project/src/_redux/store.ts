1b390862300099f3f38be403d292b26d910b4ed2
tuan <phuonghoangit2614@gmail.com>
2023-07-23 15:39:40 +0700

    switch

--- client/vite-project/src/_redux/store.ts
+++ client/vite-project/src/_redux/store.ts
@@ -1,18 +1,51 @@
 import axios from 'axios';
 import { BASE_URL } from '.';
-import { createSlice } from '@reduxjs/toolkit';
+import { configureStore, createAsyncThunk, createSlice } from '@reduxjs/toolkit';
+import { useSelector } from 'react-redux';
 
 const getlistmusic = axios.get(BASE_URL).then((res) => res.data);
 console.log(getlistmusic);
+interface IMusic {
+	_id: string;
+	title: string;
+}
+type IListMusic = IMusic[];
+interface initialState {
+	status: string;
+	list: IListMusic;
+}
 const initialState = {
 	status: 'none',
-	list: 'none',
+	list: [],
 };
-const store = createSlice({
-    name: 'haha',
-    initialState,
-    reducers: {},
-extraReducers(builder) {
-    builder.addCase
-},
-})
+
+const musicSlice = createSlice({
+	name: 'haha',
+	initialState,
+	reducers: {},
+	extraReducers(builder) {
+		builder
+			.addCase(getlistmusicAction.pending, (state, action) => {
+				state.status = 'pending';
+			})
+			.addCase(getlistmusicAction.fulfilled, (state, action) => {
+				state.status = 'fulfilled';
+				state.list = action.payload;
+			});
+	},
+});
+const store = configureStore({
+	reducer: {
+		music: musicSlice,
+	},
+});
+export default store;
+const getlistmusicAction = createAsyncThunk('haha/hehe', async () => {
+	const res = await axios.get(BASE_URL).then((res) => res.data.musics);
+	return res;
+});
+
+
+
+// const list = useSelector((state: any) => state.haha.list);
+// console.log('list in store', list);
--- client/vite-project/src/main.tsx
+++ client/vite-project/src/main.tsx
@@ -3,11 +3,11 @@ import ReactDOM from 'react-dom/client';
 import App from './App.tsx';
 import './index.css';
 import { Provider } from 'react-redux';
-// import store from './_redux/store.ts';
+import store from './_redux/store.ts';
 ReactDOM.createRoot(document.getElementById('root')!).render(
-	// <Provider store={store}>
-		// <React.StrictMode>
+	<React.StrictMode>
+		<Provider store={store}>
 			<App />
-		// </React.StrictMode>
-	// </Provider>,
+		</Provider>
+	</React.StrictMode>,
 );
