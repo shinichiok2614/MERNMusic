e56a9a10ec4f82c0a4e76931dd21ffc144defb3d
Tran Nguyen Minh Tuan <37470434+shinichiok2614@users.noreply.github.com>
2023-07-25 00:25:40 +0700

    dispatch createAsyncThunk success

--- client-vite/src/_redux/features/music.ts
+++ client-vite/src/_redux/features/music.ts
@@ -1,4 +1,6 @@
-import { createSlice } from '@reduxjs/toolkit';
+import { createAsyncThunk, createSlice, getDefaultMiddleware } from '@reduxjs/toolkit';
+import axios from 'axios';
+import { useAppSelector } from '../hooks';
 
 interface IInitState {
 	status: string;
@@ -8,10 +10,29 @@ const initialState = {
 	status: '',
 	data: '',
 };
+export const musicAsync = createAsyncThunk('music/fetchdata', async () => {
+	const getdata = () => {
+		axios.get('http://localhost:8080/api/music').then((res) => {
+			console.log(res);
+		});
+	};
+	return getdata;
+});
 const musicSlice = createSlice({
 	name: 'music',
 	initialState,
 	reducers: {},
-	extraReducers(builder) {},
+	extraReducers(builder) {
+		builder
+			.addCase(musicAsync.pending, (state, action) => {
+				state.status = 'pending';
+			})
+			.addCase(musicAsync.fulfilled, (state, action) => {
+				state.status = 'success';
+			})
+			.addCase(musicAsync.rejected, (state, action) => {
+				state.status = 'reject';
+			});
+	},
 });
-export default musicSlice.reducer
+export default musicSlice.reducer;
--- client-vite/src/_redux/store.ts
+++ client-vite/src/_redux/store.ts
@@ -1,8 +1,11 @@
-import { configureStore } from "@reduxjs/toolkit";
-import musicReducer from './features/music'
+import { configureStore } from '@reduxjs/toolkit';
+import musicReducer from './features/music';
 const store = configureStore({
-    reducer: {
-        music: musicReducer,
-    }
-}) 
-export default store
+	reducer: {
+		music: musicReducer,
+	},
+});
+export default store;
+// Infer the `RootState` and `AppDispatch` types from the store itself
+export type RootState = ReturnType<typeof store.getState>;
+export type AppDispatch = typeof store.dispatch;
